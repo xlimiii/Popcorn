a1834d749a5485ce70a69e7e0bad8633b8d4b604
Piwo13 <iworozycki@gmail.com>
2021-01-26 02:27:45 +0100

    Layouty

--- app/src/main/java/com/example/popcorn/view/HomeFragment.kt
+++ app/src/main/java/com/example/popcorn/view/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import com.example.popcorn.R
 import com.example.popcorn.viewmodel.CompanyViewModel
 import com.example.popcorn.viewmodel.MovieViewModel
