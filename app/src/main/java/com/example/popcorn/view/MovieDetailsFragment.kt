a4b9af339131d487b9373818bfc5f1d1192f8cc3
xlimiii <dominika.limanowka10@gmail.com>
2021-01-31 00:04:16 +0100

    displaying cast in movie details (sketch)

--- app/src/main/java/com/example/popcorn/view/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/popcorn/view/MovieDetailsFragment.kt
@@ -9,27 +9,43 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.popcorn.R
 import com.example.popcorn.model.Movie
 import com.example.popcorn.viewmodel.MovieViewModel
 import com.example.popcorn.viewmodel.adapters.MovieDetailsAdapter
+import kotlinx.android.synthetic.main.fragment_movie_details.*
 import kotlinx.android.synthetic.main.fragment_movie_details.view.*
 
 class MovieDetailsFragment : Fragment() {
     private lateinit var movieViewModel: MovieViewModel
+    private lateinit var movieDetailsAdapter: MovieDetailsAdapter
+    private lateinit var myLayoutManager : LinearLayoutManager
+    private lateinit var recyclerView : RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        myLayoutManager = LinearLayoutManager(context)
         movieViewModel = ViewModelProvider(requireActivity()).get(MovieViewModel::class.java)
-
-
+        movieViewModel.currentMovie.value?.id?.let {
+            movieViewModel.setPeopleConnectedWithCurrentMovie(
+                it
+            )
+        }
+        movieDetailsAdapter= MovieDetailsAdapter(movieViewModel.peopleConnectedWithCurrentMovie)
+        movieViewModel.peopleConnectedWithCurrentMovie!!.observe(viewLifecycleOwner, {movieDetailsAdapter.notifyDataSetChanged()})
         val view =  inflater.inflate(R.layout.fragment_movie_details, container, false)
 
         return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recyclerView = rv_actorsInMovie.apply {
+            this.layoutManager = myLayoutManager
+            this.adapter = movieDetailsAdapter
+        }
         view.tv_movieDetailsTitle.text = movieViewModel.currentMovie.value?.title
         view.tv_year.text = movieViewModel.currentMovie.value?.release_date
         view.tv_movieDescription.text = movieViewModel.currentMovie.value?.overview
--- app/src/main/java/com/example/popcorn/viewmodel/adapters/MovieDetailsAdapter.kt
+++ app/src/main/java/com/example/popcorn/viewmodel/adapters/MovieDetailsAdapter.kt
@@ -3,26 +3,37 @@ package com.example.popcorn.viewmodel.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.popcorn.R
 import com.example.popcorn.model.Movie
+import com.example.popcorn.model.Person
 import com.example.popcorn.viewmodel.MovieViewModel
 
 
-class MovieDetailsAdapter(val currentMovie: LiveData<Movie>, val changeCurrentMovie: (s: Movie) -> Unit) : RecyclerView.Adapter<MovieDetailsAdapter.MovieHolder>() {
+class MovieDetailsAdapter(var actorsInMovies: LiveData<List<Person>>) : RecyclerView.Adapter<MovieDetailsAdapter.MovieHolder>() {
     inner class MovieHolder(view: View): RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieDetailsAdapter.MovieHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_movie_details, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_row, parent, false)
         return MovieHolder(view)
     }
 
     override fun onBindViewHolder(holder: MovieDetailsAdapter.MovieHolder, position: Int) {
-        val movieTitle = holder.itemView.findViewById<TextView>(R.id.tv_movieDetailsTitle)
 
-        // movieTitle.text = currentMovie.value?.title
+        val name = holder.itemView.findViewById<TextView>(R.id.tv_personName)
+        val avatar = holder.itemView.findViewById<ImageView>(R.id.iv_personAvatar)
+        val actorRowBackground = holder.itemView.findViewById<LinearLayout>(R.id.actorRowBackground)
+        val url = "https://image.tmdb.org/t/p/w185${actorsInMovies.value?.get(position)?.profile_path}"
+        Glide.with(holder.itemView)
+            .load(url)
+            .centerCrop()
+            .into(avatar)
+        name.text = actorsInMovies.value?.get(position)?.name.toString()
     }
 
     override fun getItemCount(): Int {
